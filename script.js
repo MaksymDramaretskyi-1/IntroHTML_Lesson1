c8e2f61ca0a1b424524b53404f42e7680badd3d0
MaksymDramaretskyi-1 <maksym.glodedata@gmail.com>
2025-04-25 10:52:34 +0200

    fixed

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('Hello!');
+
+  const burger = document.getElementById('burger');
+  const navMenu = document.getElementById('nav-menu');
+
+  burger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+
+
